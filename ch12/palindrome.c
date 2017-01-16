6a3d6e856c6c464b73cea91c3d1f8304cebccc82
CentosOS6.4 <cloudera@localhost.localdomain>
2017-01-15 18:34:19 -0800

    started adding ch12 exercises

---
+++ ch12/palindrome.c
@@ -0,0 +1,58 @@
+/*
+ * Simple program that determines whether user-inputted
+ * string is a palindrome. Purpose is to get practice with
+ * array pointers.
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+/*typedefs*/
+typedef int bool;
+
+/*definitions*/
+#define TRUE 1
+#define FALSE 0
+#define MAX_MESSAGE 200
+
+/*function prototypes*/
+void addLetter(char c, char *arr, int *i);
+void printIsPalindrome(char *arr, int *i);
+
+int main(void)
+{
+    char chars[MAX_MESSAGE];
+    char c;
+    int i = 0;
+
+    printf("Please enter a message: ");
+
+    while((c = getchar()) != '\n')
+    {
+        if (isalpha(c))
+            addLetter(c, chars, &i);
+    }
+
+    printIsPalindrome(chars, &i);
+
+    return 0;
+}
+
+void addLetter(char c, char *arr, int *i)
+{
+    printf("%d", *i);
+
+    if(*i >= MAX_MESSAGE)
+        printf("Message too long. Program exiting...\n");
+    else
+        arr[*i++] = c;
+
+    printf("%d", *i);
+}
+
+void printIsPalindrome(char *chars, int *i)
+{
+    printf("%c", chars[0]);
+
+    printf("%c", chars[*i]);
+}
