0ddf562034b8a2e63f7bebf00c19ea6d41a2d6f6
CentosOS6.4 <cloudera@localhost.localdomain>
2017-01-21 09:47:36 -0800

    added function for parsing date string to integers

--- ch13/long_date.c
+++ ch13/long_date.c
@@ -8,11 +8,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NUM_SEGS 3
 #define DATE_SEG_MAX 4
 
 void bad_input(void);
 void print_longdate(const char *input);
-void get_date_segs(const char *input, char date_segs[3][DATE_SEG_MAX]);
+void get_date_segs(const char *input, char date_segs[NUM_SEGS][DATE_SEG_MAX+1]);
+int * parse_dates(char date_segs[NUM_SEGS][DATE_SEG_MAX+1]);
 
 int main(int argc, char *argv[])
 {
@@ -35,12 +37,14 @@ void print_longdate(const char *input)
     if(strlen(input) != 10 && strlen(input) != 9)
         bad_input();
 
-    char date_segs[3][DATE_SEG_MAX];
+    char date_segs[NUM_SEGS][DATE_SEG_MAX+1];
 
     get_date_segs(input, date_segs);
+
+    int *dates = parse_dates(date_segs);   
 }
 
-void get_date_segs(const char *input, char date_segs[3][DATE_SEG_MAX])
+void get_date_segs(const char *input, char date_segs[NUM_SEGS][DATE_SEG_MAX+1])
 {
     int i, j, num_slashes;
     i = j = num_slashes = 0;
@@ -52,9 +56,10 @@ void get_date_segs(const char *input, char date_segs[3][DATE_SEG_MAX])
 
         if(c == '/')
         {
-            j = 0;
+            date_segs[num_slashes][j] = '\0';        
+            j = 0;            
             num_slashes++;
-            if(num_slashes < 3) 
+            if(num_slashes < NUM_SEGS) 
                 continue;
             else
                 break;
@@ -70,7 +75,29 @@ void get_date_segs(const char *input, char date_segs[3][DATE_SEG_MAX])
             j++;
         }
     }
+
+    date_segs[NUM_SEGS-1][DATE_SEG_MAX] = '\0';
     
-    if(num_slashes != 2)
+    if(num_slashes != NUM_SEGS-1)
         bad_input();
+}
+
+//parses 2-d array of chars into 1-d array of ints
+int * parse_dates(char seg_dates[NUM_SEGS][DATE_SEG_MAX+1])
+{
+    static int result[NUM_SEGS];
+    int i;
+
+    for(i = 0; i < NUM_SEGS; i++)
+    {
+        int seg_val;
+        
+        if(sscanf(seg_dates[i], "%d", &seg_val) != 1)
+            bad_input();
+        
+        result[i] = seg_val;
+        printf("%d\n", result[i]);
+    }
+     
+    return result;
 }
