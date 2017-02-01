0ab076678622da8c4ebbbe9ee6872e122db2c2d9
CentosOS6.4 <cloudera@localhost.localdomain>
2017-01-31 20:06:26 -0800

    moved date parsing and validating functions to separate file

--- ch15/long_date.c
+++ ch15/long_date.c
@@ -100,36 +100,3 @@ void get_date_segs(const char *input, char date_segs[NUM_SEGS][DATE_SEG_MAX+1])
         bad_input();
 }
 
-//parses 2-d array of chars into 1-d array of ints
-int * parse_dates(char seg_dates[NUM_SEGS][DATE_SEG_MAX+1])
-{
-    static int result[NUM_SEGS];
-    int i;
-
-    for(i = 0; i < NUM_SEGS; i++)
-    {
-        int seg_val;
-        
-        if(sscanf(seg_dates[i], "%d", &seg_val) != 1)
-            bad_input();
-        
-        result[i] = seg_val;
-    }
-     
-    return result;
-}
-
-void validate_dates(int dates[NUM_SEGS])
-{
-    int m, d, y;
-    int days_per_month[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    m = dates[0];
-    d = dates[1];
-    y = dates[2];
-
-    if(m < 1 || m > 12 ||
-        d < 1 || d > 31 ||
-        y < 1000 || y > 9999 ||
-        (d > days_per_month[m-1])) 
-        bad_input();
-}
