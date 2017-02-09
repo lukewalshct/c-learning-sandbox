a6bbfe0acc16d4b4d0722c6cf340b5c734e9b1ec
CentosOS6.4 <cloudera@localhost.localdomain>
2017-02-08 16:15:44 -0800

    initial commit for ch17 exercise; added makefile

---
+++ ch17/sort_words.c
@@ -0,0 +1,15 @@
+/* 
+ * A program that sorts a series of words entered by the user,
+ * following these requirements:
+ *
+ *-Assume each word is <= 20 chars
+ *-Stop reading when user enters an empty word (i.e. just Enter)
+ *-Store each word in dynamically allocated string, using array of pointers
+ *-Sort the array and print sorted array
+ */
+
+int main(void)
+{
+
+    return 0;
+}
